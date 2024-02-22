7c5eeb83f1c9ac976e85d6d484d6b04afb4e256e
Pedro Henrique Prado <pedroh.prado@outlook.com>
2024-02-22 07:14:50 -0300

    changes for wpp API

--- src/components/Content/main.jsx
+++ src/components/Content/main.jsx
@@ -1,5 +1,8 @@
-import { Instagram, Linkedin, Phone } from 'lucide-react';
+import { Instagram, Linkedin, Phone, MessageCircleMore } from 'lucide-react';
+
 const Main = () => {
+  const number = import.meta.env.PHONE_NUMBER;
+  const message = import.meta.env.MESSAGE_DATA;
   return (
     <main className=' w-full h-1/2 mt-24 flex flex-col items-center sm:mt-28'>
       <h1 className=' text-2xl'>Camila Borges</h1>
@@ -7,10 +10,10 @@ const Main = () => {
       <ul className=' mt-10 grid grid-cols-2 gap-10'>
         <li className=' h-20 w-20 bg-[#0C2D57] rounded'>
           <a
-            href=''
+            href={`https://web.whatsapp.com/send?phone=${number}&text=${message}&app_absent=0`}
             className=' w-full h-full text-[#FC6736] flex items-center justify-center'
           >
-            w
+            <MessageCircleMore />
           </a>
         </li>
         <li className=' h-20 w-20 bg-[#0C2D57] rounded'>
